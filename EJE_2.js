95afa1593a2490f5cbfb4baacd8c8b257bb490ad
michelle_roa <roakaren2507@gmail.com>
2025-11-09 11:38:42 -0500

    se hace el segundo ejercicio de la actividad de apropiacion

---
+++ EJE_2.js
@@ -0,0 +1,10 @@
+for (let i=0;i<5; i++){
+    let carnet = prompt("El aprendiz tiene su carnet (1. Si 2. No");
+    carnet = parseInt(carnet)
+    if(carnet==1)
+        alert("El aprendiz ingreso correctamente al salon")
+    else {
+        alert("el aprendiz no ingreso al salon")
+    }
+
+}
